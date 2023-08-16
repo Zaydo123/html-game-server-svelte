40a8f049674fbb3c0c396efbf84472ad1a12d237
Zayd Alzein <zaydalzein@gmail.com>
2023-08-15 20:38:07 -0500

    shtuff

---
+++ src/routes/+error.svelte
@@ -0,0 +1,67 @@
+
+
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<link rel="stylesheet" href="/css/global.css" />
+
+<div class="container">
+	<div class="top-row">
+		<h1>{$page.status}</h1>
+	</div>
+	<div class="bottom-row">
+		<h1 id="message">{$page.error.message}</h1>
+	</div>
+</div>
+
+	
+
+
+
+
+<style>
+
+
+	h1 { 
+		color : #333;
+		background-image:  linear-gradient(144deg, #af40ff, #f9f8f9);
+    	background-size: 100%;
+		background-color: #f3ec78;
+    	-webkit-background-clip: text;
+    	-moz-background-clip: text;
+    	-webkit-text-fill-color: transparent; 
+    	-moz-text-fill-color: transparent;
+		text-align: center;
+		margin-top: 0px;
+		margin-bottom: 0px;
+	}
+	
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		height: 90vh;
+		
+	}
+
+
+	.top-row {
+	    font-size: 4em;
+	    display: flex;
+	    flex-direction: row;
+	    justify-content: center;
+	    align-items: center;
+	}
+
+
+	.bottom-row {
+
+	    font-size: 1.5em;
+		margin-top: 0;
+		padding-top: 0;
+		transform: translate(0, -60%);
+	}
+
+</style>
+
---
+++ src/routes/mirrors/+page.svelte
@@ -0,0 +1,93 @@
+<link rel="stylesheet" href="/css/global.css" />
+
+<script>
+
+  import Footer from '../components/home/footer.svelte';
+
+  export let data;
+  const { sites } = data;
+  console.log(sites);
+</script>
+
+  <style>
+
+    .container {
+      max-width: 800px;
+      margin: 50px auto;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 5px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      margin-bottom: 21%;
+    }
+  
+    h1 {
+      text-align: center;
+      color: #333;
+    }
+  
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    a {
+      text-decoration: none;
+      color: #333;
+
+      
+    }
+    a:hover {
+      text-decoration: underline;
+    }
+  
+    li {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+      border-bottom: 1px solid #eee;
+    }
+  
+    li:last-child {
+      border-bottom: none;
+    }
+  
+    .status {
+      color: green;
+      font-weight: bold;
+    }
+
+    .Online::after {
+      content: " ✔";
+    }
+
+    .Offline::after {
+      content: " ✖";
+    }
+
+    .Offline {
+      color: red;
+      background-color: #fdd;
+
+    }
+
+
+
+
+  </style>
+
+
+  <div class="container">
+    <h1>Physics Central Mirrors</h1>
+    <ul>
+      {#each sites as site, i}
+        <li>
+          <a href={site.url}><span>{site.name}</span></a>
+          <span class="status {site.status}">{sites[i].status}</span>
+        </li>
+      {/each}
+    </ul>
+  </div>
+
+
+  
+<Footer />
