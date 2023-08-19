209a09fb7856709b75f413e082e0667df4af8116
Zayd Alzein <zaydalzein@gmail.com>
2023-08-18 19:05:26 -0500

    locking down admin

--- src/routes/admin/hub/+page.server.js
+++ src/routes/admin/hub/+page.server.js
@@ -1,15 +1,17 @@
 import { db } from '$lib/db.js';
+import { verifySessionCookie } from '$lib/authUtils';
 
-//fetch all games from server (limit 150)
+
 
 function getGames() {
     return db.get('SELECT * FROM games LIMIT 150');
 }
 
 //send all games to client
-export async function load() {
-    const result = await getGames();
-    return {
-        games: result
-    };
+//dont load page if user is not authenticated
+export async function load({request}) {
+        const result = await getGames();
+        return {
+            games: result
+        };
 }
