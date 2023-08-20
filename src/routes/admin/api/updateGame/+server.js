b7a1988e804f4dc4d5cd3657a3cd86a4b353b72f
Zayd Alzein <zaydalzein@gmail.com>
2023-08-20 01:56:25 -0500

    Image handling and fixed form parsing

--- src/routes/admin/api/updateGame/+server.js
+++ src/routes/admin/api/updateGame/+server.js
@@ -1,60 +1,67 @@
 import { db } from '$lib/db';
 import { verifySessionCookie } from '$lib/authUtils';
-import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 
-// Configure multer for image upload
-const storage = multer.memoryStorage(); // Store the file in memory
-const upload = multer({ storage: storage });
+// Helper function to structure form data
+function formBody(body) {
+    return [...body.entries()].reduce((data, [k, v]) => {
+        let value = v;
+        if (v === "true") value = true;
+        if (v === "false") value = false;
+        if (!isNaN(Number(v))) value = Number(v);
+        if (k in data) {
+            const val = data[k];
+            value = Array.isArray(val) ? [...val, value] : [val, value];
+        }
+        data[k] = value;
+        return data;
+    }, {});
+}
 
-export async function POST({request}) {
+function formatDateToSQL(dateString) {
+    const date = new Date(dateString);
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+}
 
-    //TODO
-    //come back later. problems with reading request body
-    //
-    //
-    //
-
-
-
+export async function POST({ request }) {
     try {
         // First, verify if the session cookie is valid
         if (!verifySessionCookie(request)) {
-            return new Response("Not Authenticated", {status: 401, statusText: "Not Authenticated"});
+            return new Response("Not Authenticated", { status: 401, statusText: "Not Authenticated" });
         }
 
-        // Convert the readable stream to a JavaScript object
-        const rawData = await request.body.json();
-        let gameData = { ...rawData };
+        const values = await request.formData();
+        const parsedData = formBody(values);
 
-        console.log("------------------");
-        console.log(gameData);
+        // Format the date to SQL format
+        parsedData.dateAdded = formatDateToSQL(parsedData.dateAdded);
+
+        console.log('Parsed Data:', parsedData);
 
         // Handle image upload if it exists
-        if (request.files && request.files.image) {
-            const file = request.files.image;
-            const savePath = path.join(process.cwd(), 'static', 'images', 'game_images', file.originalname);
-            fs.writeFileSync(savePath, file.buffer);
-            // Update the gameData.image with the new path
-            gameData.image = `/images/game_images/${file.originalname}`;
+        if (parsedData.image) {
+            const file = parsedData.image;
+            const savePath = path.join(process.cwd(), 'static', 'images', 'game_images', file.name);
+            fs.writeFileSync(savePath, Buffer.from(await file.arrayBuffer()));
+            parsedData.image = `/images/game_images/${file.name}`;
         }
 
         // Update the game data in the database
         const result = await db.update(
             "UPDATE games SET Name = ?, Image = ?, `Date Added` = ?, Visits = ?, gamedistribution = ?, Extra = ?, Enabled = ? WHERE id = ?",
-            [gameData.name, gameData.image, gameData.dateAdded, gameData.visits, gameData.gamedistribution, gameData.extra, gameData.enabled, gameData.id]
+            [parsedData.name, parsedData.image, parsedData.dateAdded, parsedData.visits, parsedData.gamedistribution, parsedData.extra, parsedData.enabled, parsedData.id]
         );
 
-        console.log(result);
+        console.log('Result:', result);
 
-        if (result > 0) { //result = changedRows
-            return new Response("Game Updated", {status: 200, statusText: "Game Updated"});
+        if (result && result.changedRows > 0) {
+            return new Response("Game Updated", { status: 200, statusText: "Game Updated" });
         } else {
-            return new Response("Game Not Updated", {status: 500, statusText: "Game Not Updated"});
+            return new Response("Game Not Updated", { status: 500, statusText: "Game Not Updated" });
         }
     } catch (error) {
         console.error('Error updating game:', error);
-        return new Response("Error Updating Game", {status: 500, statusText: "Error Updating Game"});
+        return new Response("Error Updating Game", { status: 500, statusText: "Error Updating Game" });
     }
 }
