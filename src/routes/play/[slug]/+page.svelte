8f01e34379f1c745fc1a2abe445e1dfab056739d
Zayd Alzein <zaydalzein@gmail.com>
2023-08-26 02:39:52 -0500

    just getting started... doesnt matter for now

--- src/routes/play/[slug]/+page.svelte
+++ src/routes/play/[slug]/+page.svelte
@@ -3,7 +3,23 @@
     /** @type {import('./$types').PageData} */
     export let data;
     let game = data.props.game;
+    let gameExtra = JSON.parse(game.Extra);
+    console.log(gameExtra);
+
+    let gameSRC = "https://zayddesign.uk/" + game.ID + "/index.html";
+
+
   </script>
   
   <h1>{game.ID}</h1>
   <div>{@html game.Name}</div>
+
+
+
+  <div class="iframe-area game-{game.ID}">
+
+    <iframe src={gameSRC} frameborder="0" allowfullscreen></iframe>
+
+
+
+  </div>
