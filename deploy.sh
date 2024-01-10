fcfa3271e4bc6095f5316e1993451a2291ffd52d
Zayd Alzein <zaydalzein@gmail.com>
2024-01-09 23:22:36 -0600

    fix no-such-host

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git pull
 read -p "Enter the domain you're deploying to: " domain
 
 echo "$domain {
-    reverse_proxy svelte:app:3000 {
+    reverse_proxy svelte-app:3000 {
         header_down Strict-Transport-Security max-age=31536000;
         header_up X-Real-IP {remote}
     }
