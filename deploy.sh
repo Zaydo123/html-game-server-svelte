176ed1c2dc6e00e7bb49c40f87bfd93ebdfac0bc
Zayd Alzein <zaydalzein@gmail.com>
2024-01-09 23:29:15 -0600

    fix dep

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
-
 echo "Pulling from git..."
 git pull
 
-read -p "Enter the domain you're deploying to: " domain
+read -p "Enter the domains you're deploying to (separated by commas): " domains
 
-echo "$domain {
-    reverse_proxy svelte-app:3000 {
-        header_down Strict-Transport-Security max-age=31536000;
-        header_up X-Real-IP {remote}
-    }
-}" > Caddyfile
+IFS=',' read -ra domain_array <<< "$domains"
+
+for domain in "${domain_array[@]}"; do
+    echo "$domain {
+        reverse_proxy localhost:3000 {
+            header_down Strict-Transport-Security max-age=31536000;
+            header_up X-Real-IP {remote}
+        }
+    }" >> Caddyfile
+done
 
 echo "Building..."
 docker-compose up -d --build
