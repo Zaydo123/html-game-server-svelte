5a983797df37104e85e8e73578ec1710ab5081f5
Zayd Alzein <zaydalzein@gmail.com>
2024-01-09 22:52:10 -0600

    caddy transition

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+
+echo "Pulling from git..."
+git pull
+
+read -p "Enter the domain you're deploying to: " domain
+
+echo "$domain {
+    reverse_proxy node:app:3000 {
+        header_down Strict-Transport-Security max-age=31536000;
+        header_up X-Real-IP {remote}
+    }
+}" > Caddyfile
+
+echo "Building..."
+docker-compose up -d --build
+
+echo "Done."
