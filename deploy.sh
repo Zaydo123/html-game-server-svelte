7f103eea44e43b7f7c061506ed7694ce92ba4854
Zayd Alzein <zaydalzein@gmail.com>
2024-01-09 23:30:59 -0600

    fix dep - multi domain

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ IFS=',' read -ra domain_array <<< "$domains"
 
 for domain in "${domain_array[@]}"; do
     echo "$domain {
-        reverse_proxy localhost:3000 {
+        reverse_proxy svelte-app:3000 {
             header_down Strict-Transport-Security max-age=31536000;
             header_up X-Real-IP {remote}
         }
